75bef0bc617a2af345544851528bd3d84408a7b8
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-05-13 19:35:57 +0200

    Initial Backend Setup
    Typescript, Express, NodeJS, GraphQL, Apollo, Google-Auth

---
+++ Backend/src/server.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import depthLimit from "graphql-depth-limit"; // https://www.npmjs.com/package/graphql-depth-limit
+import { createServer } from "http";
+// Gzip compressing can greatly decrease the size of the response body and hence increase the speed of a web app.
+// https://expressjs.com/en/advanced/best-practice-performance.html
+import compression from "compression";
+import cors from "cors";
+import schema from "./schema";
+
+const app = express();
+
+const server = new ApolloServer({
+  schema,
+  validationRules: [depthLimit(7)], // see import
+});
+
+// Additional middleware can be mounted at this point to run before Apollo.
+// CORS MIDDLEWARE
+app.use("*", cors());
+// COMPRESSION MIDDLEWARE
+app.use(compression()); // see import
+
+server.applyMiddleware({ app, path: "/graphql" }); // Mount Apollo middleware here. If no path is specified, it defaults to `/graphql`.
+
+const httpServer = createServer(app);
+
+httpServer.listen({ port: 3000 }, (): void =>
+  console.log(
+    `\n🚀      GraphQL is now running on http://localhost:3000/graphql`
+  )
+);
