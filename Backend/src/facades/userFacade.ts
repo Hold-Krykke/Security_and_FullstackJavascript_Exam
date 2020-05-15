720805a6d65efc5d5c751fd420adeb96c3a49afb
Camilla Staunstrup <camilla.valerius@gmail.com>
2020-05-15 14:40:54 +0200

    ugly hole through

---
+++ Backend/src/facades/userFacade.ts
@@ -0,0 +1,46 @@
+const path = require('path')
+require('dotenv').config({ path: path.join(process.cwd(), '.env') })
+import * as mongo from 'mongodb'
+import IUser from '../interfaces/IUser';
+const bcrypt = require('bcryptjs');
+import setup from '../config/setupDB'
+
+
+
+let userCollection: mongo.Collection;
+
+export default class UserFacade {
+    static async setDatabase(client: mongo.MongoClient) {
+        try {
+            if (!client.isConnected()) {
+                await client.connect();
+            }
+            userCollection = client.db().collection("users");
+            await userCollection.createIndex({ userName: 1 }, { unique: true })
+            return client.db();
+
+        } catch (err) {
+            console.error("Could not create connect", err)
+        }
+    }
+
+    static async addUser(user: IUser): Promise<boolean> {
+        const hash: String = await new Promise((resolve, reject) => {
+            bcrypt.hash(user.password, 10, (err: Error, hash: string) => {
+                if (err) {
+                    reject(err);
+                }
+                return resolve(hash);
+            });
+        });
+
+        let newUser = { ...user, password: hash }
+        try {
+            await userCollection.insertOne(newUser);
+            return true;
+        } catch (err) {
+            console.log(err.errmsg)
+            return false;
+        }
+    }
+}
--- Backend/src/resolverMap.ts
+++ Backend/src/resolverMap.ts
@@ -1,5 +1,14 @@
 import { IResolvers } from "graphql-tools";
 
+const path = require('path')
+require('dotenv').config({ path: path.join(process.cwd(), '.env') })
+//require('dotenv').config();
+import * as mongo from 'mongodb'
+import IUser from './interfaces/IUser';
+const bcrypt = require('bcryptjs');
+import setup from './config/setupDB'
+import UserFacade from './facades/userFacade'
+
 // Resolvers
 // Used in Schema to make a GraphQL schema
 // Schema is used to make Apollo Server
@@ -9,12 +18,22 @@ type Query {
   helloWorld: String!
 }
 */
+
+(async function setupDB() {
+    console.log(path.join(process.cwd(), '.env'))
+    let testuser: IUser = { userName: 'hest', password: 'hat' }
+    const client = await setup()
+    UserFacade.setDatabase(client)
+    console.log(UserFacade.addUser(testuser))
+
+})()
+
 const resolverMap: IResolvers = {
-  Query: {
-    helloWorld(_: void, args: void): string {
-      return `Hello world!`;
+    Query: {
+        helloWorld(_: void, args: void): string {
+            return `Hello world!`;
+        },
     },
-  },
 };
 
 export default resolverMap;
