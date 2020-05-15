d385c57f5a73a159be53697f06a06104e74ec760
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-05-15 14:11:33 +0200

    using environment variables

--- Backend/src/passportSetup.ts
+++ Backend/src/passportSetup.ts
@@ -1,17 +1,22 @@
 import passport from "passport";
-
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const path = require("path");
+require("dotenv").config({ path: path.join(process.cwd(), ".env") });
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID, // "<GOOGLE_CLIENT_ID>", // e.g. asdfghjkljhgfdsghjk.apps.googleusercontent.com
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET, //"<GOOGLE_CLIENT_SECRET>", // e.g. _ASDFA%DFASDFASDFASD#FAD-
+      // redirect: "https://your-website.com/google-auth", // this must match your google api settings
       callbackURL: "http://www.example.com/auth/google/callback",
     },
-    function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
+    function (accessToken: any, refreshToken: any, profile: any, done: any) {
+      User.findOrCreate({ googleId: profile.id }, function (
+        err: any,
+        user: any
+      ) {
+        return done(err, user);
       });
     }
   )
