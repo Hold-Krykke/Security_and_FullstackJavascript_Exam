09d52c8040991ee7d714a7add6bac2ad738d075e
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-05-15 18:18:02 +0200

    This branch is a mess.

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -10,8 +10,33 @@ import bodyParser from "body-parser";
 
 const app = express();
 
+// This block of code is setting up a new GraphQL server, using Apollo Server 2.0.
+//This new version of Apollo Server simplifies the API for creating new servers, and has some more intelligent defaults.
+// You can read more about it here! https://www.apollographql.com/blog/apollo-server-2-0-30c9bbb4ab5e
 const server = new ApolloServer({
   schema,
+  context: ({ req }) => {
+    // Note! This example uses the `req` object to access headers,
+    // but the arguments received by `context` vary by integration.
+    // This means they will vary for Express, Koa, Lambda, etc.!
+    //
+    // To find out the correct arguments for a specific integration,
+    // see the `context` option in the API reference for `apollo-server`:
+    // https://www.apollographql.com/docs/apollo-server/api/apollo-server/
+
+    // get the user token from the headers
+    const token = req.headers.authorization || "";
+
+    // try to retrieve a user with the token
+    const user = getUser(token);
+
+    // optionally block the user
+    // we could also check user roles/permissions here
+    if (!user) throw new AuthenticationError("you must be logged in");
+
+    // add the user to the context
+    return { user };
+  },
   validationRules: [depthLimit(7)], // see import
 });
 
