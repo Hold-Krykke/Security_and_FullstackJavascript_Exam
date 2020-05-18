ac5af60153c4cfa8abc10b202edc2120c87df196
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-05-18 11:03:39 +0200

    Needs serious cleanup in server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -3,9 +3,6 @@ import { ApolloServer, AuthenticationError } from "apollo-server-express";
 import depthLimit from "graphql-depth-limit"; // https://www.npmjs.com/package/graphql-depth-limit
 // Gzip compressing can greatly decrease the size of the response body and hence increase the speed of a web app.
 // https://expressjs.com/en/advanced/best-practice-performance.html
-import compression from "compression";
-import cors from "cors";
-import schema from "./schema";
 import session from "express-session";
 import { v4 as uuid } from "uuid";
 import passport from "passport";
@@ -16,10 +13,10 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passportJWT from "passport-jwt";
 import jwt from "jsonwebtoken";
-import compression from 'compression';
-import cors from 'cors';
-import schema from './schema';
-import { ApiError } from './customErrors/apiError';
+import compression from "compression";
+import cors from "cors";
+import schema from "./schema";
+import { ApiError } from "./customErrors/apiError";
 import authMiddleware from "./middlewares/basicAuth";
 
 const SESSION_SECRECT = "bad secret";
@@ -125,23 +122,20 @@ const server = new ApolloServer({
     },
   },
 });
-server.applyMiddleware({ app, cors: false, path: "/graphql" }); // Mount Apollo middleware here. If no path is specified, it defaults to `/graphql`.
-app.use(authMiddleware)
+app.use(authMiddleware);
 
+app.use(function (err: any, req: any, res: any, next: Function) {
+  if (err instanceof ApiError) {
+    const e = <ApiError>err;
+    return res
+      .status(e.errorCode)
+      .send({ code: e.errorCode, message: e.message });
+  }
+  next(err);
+});
+server.applyMiddleware({ app, cors: false, path: "/graphql" }); // Mount Apollo middleware here. If no path is specified, it defaults to `/graphql`.
 app.listen({ port: PORT }, (): void =>
   console.log(
     `\n\nGraphQL is now running on http://localhost:${PORT}/graphql\n`
   )
-server.applyMiddleware({ app, path: '/graphql' }); // Mount Apollo middleware here. If no path is specified, it defaults to `/graphql`.
-
-app.use(function (err: any, req: any, res: any, next: Function) {
-    if (err instanceof (ApiError)) {
-        const e = <ApiError>err;
-        return res.status(e.errorCode).send({ code: e.errorCode, message: e.message })
-    }
-    next(err)
-})
-
-app.listen({ port: 3000 }, (): void =>
-    console.log(`\n\nGraphQL is now running on http://localhost:3000/graphql\n`)
 );
