3cc2716be2e35ce993f87e4cfe1477b9279ff069
Runi <Runiniclasen@hotmail.com>
2020-05-19 18:57:17 +0200

    whitelist, log IP

--- Backend/src/middlewares/logger.ts
+++ Backend/src/middlewares/logger.ts
@@ -14,21 +14,24 @@ if (process.env.NODE_ENV !== 'production') {
     //if dev, also log to console
     requestLoggerTransports.push(new winston.transports.Console({
         format: format.combine(
-            format.colorize(),
+            //format.colorize({ all: true }),
             format.json(),
             format.simple(),
+            format.prettyPrint()
             //format.splat()
         ),
     }));
     errorLoggerTransports.push(new winston.transports.Console({
         format: format.combine(
-            format.colorize(),
+            //format.colorize({ all: true }),
             format.json(),
-            format.errors({ stack: true }), //print stacktrace to console
-            format.simple()
+            //format.errors({ stack: true }), //print stacktrace to console
+            format.simple(),
+            format.prettyPrint()
         )
     }));
 }
+
 
 let requestLogger = expressWinston.logger({
     transports: requestLoggerTransports,
@@ -37,10 +40,10 @@ let requestLogger = expressWinston.logger({
         format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     ),
     expressFormat: true, //may overwrite some of the above
-    colorize: true, //may be overwritten
-    baseMeta: { request: "HTTP {{req.method}} {{res.responseTime}}ms {{req.url}} {{req.ip}}" },
-    meta:true,
-
+    colorize: false,
+    meta: true,
+    requestWhitelist: [...expressWinston.requestWhitelist, "ip"], //ip is formatted ipv6:ipv4
+    ignoreRoute: (req) => (req.url === '/graphql')
 })
 
 let errorLogger = expressWinston.errorLogger({
@@ -49,6 +52,7 @@ let errorLogger = expressWinston.errorLogger({
         format.json(),
         format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' })
     ),
+
 })
 
 export { requestLogger, errorLogger };
