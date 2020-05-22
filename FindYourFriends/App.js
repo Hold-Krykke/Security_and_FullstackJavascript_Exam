61f2dba17bdad8287b3fc1b14e98683b96cff317
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-05-22 13:42:42 +0200

    Update App.js

--- FindYourFriends/App.js
+++ FindYourFriends/App.js
@@ -20,6 +20,7 @@ export default function App() {
   const httpLink = createHttpLink({ uri: backendUri + "/graphql" });
   const client = new ApolloClient({
     uri: backendUri,
+    // httpLink and cache are requirements as of Apollo 2
     link: errorLink.concat(authLink.concat(httpLink)), // Add in when links are made properly // Some kinda apollo middleware. See ./utils/links // https://www.apollographql.com/docs/link/links/error/
     cache: new InMemoryCache(), // automatic caching of requests of the same data // https://www.apollographql.com/docs/angular/basics/caching/
   });
